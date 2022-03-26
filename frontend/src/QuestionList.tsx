b60eb589ed49831171891a56e97464540b1d008a
Darwish98 <66258375+Darwish98@users.noreply.github.com>
2022-03-26 19:07:01 +0100

    added question and questionslist components

---
+++ frontend/src/QuestionList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { QuestionData } from './QuestionsData';
+interface Props {
+  data: QuestionData[];
+}
+export const QuestionList = ({ data }: Props) => (
+  <ul>
+    {data.map((question) => (
+      <li key={question.questionId}></li>
+    ))}
+  </ul>
+);
