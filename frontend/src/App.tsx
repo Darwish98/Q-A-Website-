9aa1331ac90acbb828f0efa6663ccc8e0ce1924a
Darwish98 <66258375+Darwish98@users.noreply.github.com>
2022-03-26 23:15:36 +0100

    Added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,54 @@ import { fontFamily, fontSize, gray2 } from './Styles';
 import { css } from '@emotion/react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
+import { NotFoundPage } from './NotFoundPage';
+import { QuestionPage } from './QuestionPage';
+import React from 'react';
+const AskPage = React.lazy(() => import('./AskPage'));
 
 function App() {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Routes>
+          <Route path="questions/:questionId" element={<QuestionPage />} />
+          <Route path="" element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route
+            path="ask"
+            element={
+              <React.Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </React.Suspense>
+            }
+          />
+
+          <Route path="signin" element={<SignInPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
