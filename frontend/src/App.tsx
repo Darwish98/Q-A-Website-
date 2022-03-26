ca90513cf8e6930457e4ab24caf31e463e8fbf68
Darwish98 <66258375+Darwish98@users.noreply.github.com>
2022-03-26 21:40:51 +0100

    Added styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { fontFamily, fontSize, gray2 } from './Styles';
+import { css } from '@emotion/react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 
 function App() {
   return (
-    <div className="App">
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
       <Header />
       <HomePage />
     </div>
