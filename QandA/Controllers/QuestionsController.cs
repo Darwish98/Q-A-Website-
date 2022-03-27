4e3681a6bccb4c8e6846af7bcbcfb00104f32b4a
Darwish98 <66258375+Darwish98@users.noreply.github.com>
2022-03-27 16:39:00 +0200

    added more method in QuestionsController

--- QandA/Controllers/QuestionsController.cs
+++ QandA/Controllers/QuestionsController.cs
@@ -34,5 +34,78 @@ namespace QandA.Controllers
                 _dataRepository.GetQuestionsBySearch(search);
             }
         }
+
+        [HttpGet("unanswered")]
+        public IEnumerable<QuestionGetManyResponse>GetUnansweredQuestions()
+        {
+            return _dataRepository.GetUnansweredQuestions();
+        }
+
+        [HttpGet("{questionId}")]
+        public ActionResult<QuestionGetSingleResponse>GetQuestion(int questionId)
+        {
+            var question= _dataRepository.GetQuestion(questionId);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+            return question;
+        }
+
+        [HttpPost]
+        public ActionResult<QuestionGetSingleResponse>PostQuestion(QuestionPostRequest questionPostRequest)
+        {
+            var savedQuestion = _dataRepository.PostQuestion(questionPostRequest);
+            return CreatedAtAction(nameof(GetQuestion),
+                new { questionId = savedQuestion.QuestionId },
+                savedQuestion);
+
+        }
+
+        [HttpPut("{questionId}")]
+        public ActionResult<QuestionGetSingleResponse>PutQuestion(int questionId,QuestionPutRequest questionPutRequest)
+        {
+            var question=_dataRepository.GetQuestion(questionId);
+            if (question == null)
+            {
+                return NotFound();
+            }
+            questionPutRequest.Title =string.IsNullOrEmpty(questionPutRequest.Title) ?
+                question.Title :
+                questionPutRequest.Title;
+            questionPutRequest.Content =string.IsNullOrEmpty(questionPutRequest.Content) ?
+                question.Content :
+                questionPutRequest.Content;
+
+            var savedQuestion = _dataRepository.PutQuestion(questionId,questionPutRequest);
+            return savedQuestion;
+        }
+
+        [HttpDelete("{questionId}")]
+        public ActionResult DeleteQuestion(int questionId)
+        {
+            var question =_dataRepository.GetQuestion(questionId);
+            if (question == null)
+            {
+                return NotFound();
+            }
+            _dataRepository.DeleteQuestion(questionId);
+            return NoContent();
+        }
+
+        [HttpPost("answer")]
+        public ActionResult<AnswerGetResponse>PostAnswer(AnswerPostRequest answerPostRequest)
+        {
+            var questionExists =_dataRepository.QuestionExists(answerPostRequest.QuestionId);
+            if (!questionExists)
+            {
+                return NotFound();
+            }
+            var savedAnswer =_dataRepository.PostAnswer(answerPostRequest);
+            return savedAnswer;
+        }
+
     }
+    
 }
